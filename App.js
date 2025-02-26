f5c374d879b0f1669a12ad3292e437d6e46fd7dd
Douglas Santos <douglaspereirapdj@gmail.com>
2025-02-26 19:11:21 -0300

    agora vai

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/pages/homepage'
+
+const App = () => {
+
+  return (
+
+   <HomePage />
+  
+  );
+
+}
+
+export default App;
